a99b7e35823e5f1c08cc47e55c122db74fe970aa
Alex Debril <alex.debril@gmail.com>
2013-02-06 01:35:39 +0100

    fix path problem under vendor

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,8 +8,7 @@
  * @copyright (c) 2013, Alexandre Debril
  *
  */
-var_dump(__DIR__);
-$file = __DIR__.'/../../../../vendor/autoload.php';
+$file = __DIR__.'/../../../../../../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
