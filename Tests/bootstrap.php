a48ec14c565240e033c8e29de45a252f028d639a
Alex Debril <alex.debril@gmail.com>
2013-02-06 01:51:56 +0100

    Tests can be launch in travis

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,6 +9,10 @@
  *
  */
 $file = __DIR__.'/../../../../../../vendor/autoload.php';
+if (!file_exists($file)){
+    $file = __DIR__.'/../vendor/autoload.php';
+}
+
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
 }
