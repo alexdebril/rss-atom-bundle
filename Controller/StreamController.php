76d87eda0ec2b430484ccd65ea118d044f8985d2
Alex Debril <alex.debril@gmail.com>
2013-06-06 09:33:04 +0200

    Multi-formats and options support

--- Controller/StreamController.php
+++ Controller/StreamController.php
@@ -4,6 +4,7 @@ namespace Debril\RssAtomBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\OptionsResolver\Options;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Debril\RssAtomBundle\Provider\FeedContentProvider;
@@ -15,12 +16,24 @@ class StreamController extends Controller
      * @Route("/stream/{contentId}")
      * @Template()
      */
-    public function indexAction($contentId)
+    public function indexAction($format, $contentId = null)
     {
+        $options = new Options;
+        if (!is_null($contentId))
+            $options->set('contentId', $contentId);
 
-        $formatter = $this->getFormatter();
+        return $this->createStreamResponse($options, $format);
+    }
 
-        $content = $this->getContent($contentId);
+    /**
+     *
+     * @param \Symfony\Component\OptionsResolver\Options $options
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createStreamResponse(Options $options, $format)
+    {
+        $formatter = $this->getFormatter($format);
+        $content = $this->getContent($options);
 
         $response = new Response($formatter->toString($content));
         $response->headers->set('Content-Type', 'application/xhtml+xml');
@@ -30,9 +43,11 @@ class StreamController extends Controller
 
     /**
      *
-     * @param mixed $contentId
+     * @param \Symfony\Component\OptionsResolver\Options $options
+     * @return FeedContent
+     * @throws \Exception
      */
-    protected function getContent($contentId)
+    protected function getContent(Options $options)
     {
         $provider = $this->get('FeedContentProvider');
 
@@ -40,16 +55,26 @@ class StreamController extends Controller
         {
             throw new \Exception('Provider is not a FeedContentProvider instance');
         }
-        return $provider->getFeedContentById($contentId);
+        return $provider->getFeedContentById($options);
     }
 
     /**
      *
+     * @param  string $format
      * @return Debril\RssAtomBundle\Protocol\FeedFormatter
+     * @throws Exception
      */
-    protected function getFormatter()
+    protected function getFormatter($format)
     {
-        return $this->get('FeedFormatter');
+        $services = array(
+            'rss' => 'FeedFormatter',
+            'atom' => 'FeedFormatter',
+        );
+
+        if (!array_key_exists($format, $services))
+            throw new Exception("Unsupported format {$format}");
+
+        return $this->get($services[$format]);
     }
 
 }
