d2082956a5a3e183566e45055f33634bcba310a7
Alex Debril <alex.debril@gmail.com>
2013-07-11 17:49:31 +0200

    Documentation

--- Protocol/FeedIn.php
+++ Protocol/FeedIn.php
@@ -21,49 +21,58 @@ interface FeedIn
 {
 
     /**
-     *
+     * Atom : feed.entry <feed><entry>
+     * Rss  : rss.channel.entry <rss><channel><entry>
      * @param \Debril\RssAtomBundle\Protocol\ItemIn $item
      */
     public function addItem(ItemIn $item);
 
     /**
-     *
+     * Atom : feed.entry <feed><entry>
+     * Rss  : rss.channel.entry <rss><channel><entry>
      * @return array[\Debril\RssAtomBundle\Protocol\ItemIn]
      */
     public function getItems();
 
     /**
-     *
+     * Atom : feed.updated <feed><updated>
+     * Rss  : rss.channel.lastBuildDate <rss><channel><lastBuildDate>
      * @param \DateTime $lastModified
      */
     public function setLastModified(\DateTime $lastModified);
 
     /**
-     *
+     * Atom : feed.title <feed><title>
+     * Rss  : rss.channel.title <rss><channel><title>
      * @param string $title
      */
     public function setTitle($title);
 
     /**
-     *
+     * Atom : feed. <feed><subtitle>
+     * Rss  : rss.channel. <rss><channel><description>
      * @param string $description
      */
     public function setDescription($description);
 
     /**
-     *
+     * Atom : feed.link <feed><link>
+     * Rss  : rss.channel.link <rss><channel><link>
      * @param string $link
      */
     public function setLink($link);
 
     /**
+     * Atom : feed.id <feed><id>
+     * Rss  : rss.channel.id <rss><channel><id>
      * @deprecated
      * @param mixed $id
      */
     public function setId($id);
 
     /**
-     *
+     * Atom : feed.id <feed><id>
+     * Rss  : rss.channel.id <rss><channel><id>
      * @param string $id
      */
     public function setPublicId($id);
