2e4895e91a886ad6f3682763a5c7a50e9a090972
Alex Debril <alex.debril@gmail.com>
2014-04-25 23:53:14 +0200

    Duplication removal

---
+++ Tests/Protocol/FormatterAbstract.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 25/04/14
+ * Time: 23:44
+ */
+
+namespace Debril\RssAtomBundle\Tests\Protocol;
+
+use \Debril\RssAtomBundle\Protocol\Parser\FeedContent;
+use \Debril\RssAtomBundle\Protocol\Parser\Item;
+
+class FormatterAbstract extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var FeedContent
+     */
+    protected $feed;
+
+
+    protected function setUp()
+    {
+        $this->feed = new FeedContent();
+
+        $this->feed->setPublicId('feed id');
+        $this->feed->setLink('http://example.com');
+        $this->feed->setTitle('feed title');
+        $this->feed->setDescription('feed subtitle');
+        $this->feed->setLastModified(new \DateTime);
+
+        $item = new Item;
+        $item->setPublicId('item id');
+        $item->setLink('http://example.com/1');
+        $item->setSummary('lorem ipsum');
+        $item->setTitle('title 1');
+        $item->setUpdated(new \DateTime);
+        $item->setComment('http://linktothecomments.com');
+        $item->setAuthor('Contributor');
+
+        $this->feed->addItem($item);
+    }
+
+    /**
+     * @param $object
+     */
+    protected function _testSetEntries($object)
+    {
+        $element = $object->getRootElement();
+
+        $object->setEntries($element, $this->feed);
+
+        foreach ($element->childNodes as $entry)
+        {
+            $this->assertInstanceOf("\DomNode", $entry);
+        }
+    }
+
+} 
