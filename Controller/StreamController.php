f02647131397b1c61e40126586a9f6f4c9df9a0a
Alex Debril <alex.debril@gmail.com>
2015-05-31 19:59:55 +0200

    Removed use statement

--- Controller/StreamController.php
+++ Controller/StreamController.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Debril\RssAtomBundle\Provider\FeedContentProviderInterface;
 use Debril\RssAtomBundle\Exception\FeedNotFoundException;
 
