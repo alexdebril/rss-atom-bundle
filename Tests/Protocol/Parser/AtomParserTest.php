c3dc74ab23260509e0785dcc49469e9620a9b844
Romaric Drigon <romaric@netinfluence.ch>
2015-06-24 18:35:17 +0200

    Parsing Atom categories

--- Tests/Protocol/Parser/AtomParserTest.php
+++ Tests/Protocol/Parser/AtomParserTest.php
@@ -107,6 +107,12 @@ class AtomParserTest extends ParserAbstract
         }
 
         $this->assertEquals(1, $count);
+
+        $categories = $item->getCategories();
+        $this->assertCount(2, $categories);
+        $this->assertInstanceOf('Debril\RssAtomBundle\Protocol\Parser\Category', $categories[0]);
+        $this->assertEquals('Category1', $categories[0]->getName());
+        $this->assertEquals('Category2', $categories[1]->getName());
     }
 
     /**
