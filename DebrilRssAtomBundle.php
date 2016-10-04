6134104c92276e5f52a53094b1a880c8bb41e9c3
Alex Debril <alex.debril@gmail.com>
2016-10-05 00:18:38 +0200

    Remove unnecessary use statements

--- DebrilRssAtomBundle.php
+++ DebrilRssAtomBundle.php
@@ -2,9 +2,7 @@
 
 namespace Debril\RssAtomBundle;
 
-use Debril\RssAtomBundle\DependencyInjection\CompilerPass\DriverCompilerPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class DebrilRssAtomBundle extends Bundle
 {
