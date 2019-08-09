c3181f27b6f1d68ecf70b47ce008f97d4f6ac019
Alex Debril <alex.debril@gmail.com>
2019-08-09 11:33:14 +0200

    Backward compatibility with both Provider interfaces

--- Controller/StreamController.php
+++ Controller/StreamController.php
@@ -2,10 +2,12 @@
 
 namespace Debril\RssAtomBundle\Controller;
 
+use Debril\RssAtomBundle\Provider\FeedProviderInterface;
 use Debril\RssAtomBundle\Request\ModifiedSince;
 use Debril\RssAtomBundle\Provider\FeedContentProviderInterface;
 use Debril\RssAtomBundle\Exception\FeedException\FeedNotFoundException;
 use Debril\RssAtomBundle\Response\FeedBuilder;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -38,20 +40,23 @@ class StreamController
 
     /**
      * @param Request $request
-     * @param FeedContentProviderInterface $provider
+     * @param FeedProviderInterface $provider
+     * @param LoggerInterface $logger
      * @return Response
-     * @throws \Exception
      */
-    public function indexAction(Request $request, FeedContentProviderInterface $provider) : Response
+    public function indexAction(Request $request, FeedProviderInterface $provider, LoggerInterface $logger) : Response
     {
-        $options = $request->attributes->get('_route_params');
-        $options['Since'] = $this->modifiedSince->getValue();
-
         try {
-            return $this->feedBuilder->getResponse(
-                $request->get('format', 'rss'),
-                $provider->getFeedContent($options)
-            );
+            if ($provider instanceof FeedContentProviderInterface) {
+                $logger->info('The \\Debril\\RssAtomBundle\\Provider\\FeedContentProviderInterface is deprecated since rss-atom-bundle 4.3, use FeedProviderInterface instead');
+                $options = $request->attributes->get('_route_params');
+                $options['Since'] = $this->modifiedSince->getValue();
+                $feed = $provider->getFeedContent($options);
+            } else {
+                $feed = $provider->getFeed($request);
+            }
+
+            return $this->feedBuilder->getResponse($request->get('format', 'rss'), $feed);
         } catch (FeedNotFoundException $e) {
             throw new NotFoundHttpException('feed not found');
         }
